84ef2989e9e90a6658ef076e296758b38ad1c089
Gabrielle <gabrielleajornalist@gmailcom>
2023-06-27 21:53:49 -0300

    first commit

---
+++ exemplo2.js
@@ -0,0 +1,20 @@
+function mensagem() {
+    const nome = 'Gabrielle Andrade'
+    document.write('Seja Bem Vindo' + nome); //contatenar - juntar textos
+
+}
+
+function soma(num1, num2) {
+    document.write(`A soma de ${num1} e ${num2} é: ${num1 + num2}`)
+
+}
+function mult(num1,num2){
+    return num1 * num2
+}
+
+mensagem()
+document.writer('<br>')
+soma(10, 15)
+document.writer('<br>')
+soma(5, 10)
+document.write(mult(5,10))
